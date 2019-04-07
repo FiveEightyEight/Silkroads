d00a530a528d8a0117ee240672f9238a5aa42f9c
Robert Abreu <robertabreu@pursuit.org>
2019-04-06 22:29:42 -0400

    created functional component BottomNav

---
+++ src/components/BottomNav.js
@@ -0,0 +1,29 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import { BottomNavigation, BottomNavigationAction, } from '@material-ui/core/';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import Folder from '@material-ui/icons/Folder';
+
+const styles = {
+    root: {
+        width: '100%',
+    },
+};
+
+export default withStyles(styles)( props => {
+    const { classes: {root}, value, handleChange } = props;
+    return (
+        <BottomNavigation value={value} onChange={handleChange} className={root}>
+            <BottomNavigationAction label="Recents" value="recents" icon={<RestoreIcon />} />
+            <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
+            <BottomNavigationAction label="Folder" value="folder" icon={<Folder />} />
+        </BottomNavigation>
+    );
+});
+
+// BottomNav.propTypes = {
+//   classes: PropTypes.object.isRequired,
+// };
+
