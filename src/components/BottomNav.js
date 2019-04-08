c48abb5dd72d40e062b02c8745f3f3403cc9cf04
Robert Abreu <robertabreu@pursuit.org>
2019-04-08 18:47:56 -0400

    BottomNav will remain in the home component

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import { BottomNavigation, BottomNavigationAction, } from '@material-ui/core/';
+import { BottomNavigation, BottomNavigationAction, AppBar, } from '@material-ui/core/';
 import RestoreIcon from '@material-ui/icons/Restore';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import Folder from '@material-ui/icons/Folder';
@@ -10,16 +10,20 @@ const styles = {
     root: {
         width: '100%',
     },
+    bottomBar: {
+        top: 'auto',
+        bottom: 0,
+      },
 };
 
-export default withStyles(styles)( props => {
-    const { classes: {root}, value, handleChange } = props;
+export default withStyles(styles)(props => {
+    const { classes: { root }, value, handleChange } = props;
     return (
-        <BottomNavigation value={value} onChange={handleChange} className={root}>
-            <BottomNavigationAction label="Recents" value="recents" icon={<RestoreIcon />} />
-            <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
-            <BottomNavigationAction label="Folder" value="folder" icon={<Folder />} />
-        </BottomNavigation>
+            <BottomNavigation value={value} onChange={handleChange} className={root} position='sticky'>
+                <BottomNavigationAction label="Recents" value="recents" icon={<RestoreIcon />} />
+                <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
+                <BottomNavigationAction label="Folder" value="folder" icon={<Folder />} />
+            </BottomNavigation>
     );
 });
 
