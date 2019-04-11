19353919b1fb7bded5afc6e15341b1f75b161855
Robert Abreu <robertabreu@pursuit.org>
2019-04-11 11:26:23 -0400

    minor feed height adjustment

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,22 +3,37 @@ import { withStyles } from '@material-ui/core/styles';
 import { Grid, TextField, IconButton, Paper, Typography, Button, } from '@material-ui/core';
 
 // Context
-import AuthContext from './contexts/Auth';
 
-const styles = theme => {
 
-};
+const styles = theme => ({
 
-export default withStyles(styles)( props => {
-
-    getFeed = () => {
-        
+    block: {
+        marginTop: '5px',
+        marginBottom: '5px',
     }
+    
+});
 
+export default withStyles(styles)(props => {
+    const { classes } = props;
     return (
-        <Grid container>
+        <Grid container className={classes.block} >
             <Grid item xs={12}>
-                
+                <Paper>
+                    <Grid container>
+                        <Grid item xs={12}>
+                            <Typography variant='h5'
+                            >
+                                Title Goes Here
+                            </Typography>
+                        </Grid>
+                            <Typography variant='body1'
+                            >
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum reiciendis accusamus aperiam quisquam fugit eum, 
+                                temporibus dolorem ducimus. Reprehenderit cum molestiae commodi sit tenetur incidunt, quisquam alias voluptas quidem laborum.
+                            </Typography>
+                    </Grid>
+                </Paper>
             </Grid>
         </Grid>
     )
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,19 +2,33 @@ import React, { Component } from 'react';
 import Login from './Login'
 import BottomNav from '../components/BottomNav';
 import { withStyles } from '@material-ui/core/styles';
-import { Grid } from '@material-ui/core';
-
+import { Grid, Paper } from '@material-ui/core';
+import Feed from '../components/Feed';
 // Contexts
 import { Consumer } from '../contexts/Auth';
 const styles = theme => ({
     home: {
         height: '100%',
     },
+    paper: {
+        height:'250px',
+        // height: 'calc(100% - 250px)',
+        overflowY: 'auto',
+    //     [theme.breakpoints.up('sm')]: {
+    //         marginTop: 5,
+    //         height: 'calc(100% - 10px)'
+    //     },
+    //     [theme.breakpoints.down('xs')]: {
+    //         height: '100%'
+    //     },
+    },
+
 });
 
 export default withStyles(styles)(class Home extends Component {
     state = {
         page: 'recents',
+
     }
 
     handleChange = (event, page) => {
@@ -34,9 +48,14 @@ export default withStyles(styles)(class Home extends Component {
                                 >
                                     <h2>Welcome back, {user.email}</h2>
                                     <h4>Your ID is: {user.uid}</h4>
-                                    <div style={{ height: '1000px' }}>
 
-                                    </div>
+                                    <Paper className={classes.paper}>
+                                        {
+                                            [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map((e, i) => {
+                                                return <Feed key={i} />
+                                            })
+                                        }
+                                    </Paper>
                                     <BottomNav value={this.state.page} handleChange={this.handleChange} />
                                 </Grid>
 
