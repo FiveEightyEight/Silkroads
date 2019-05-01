643ac6649ff4cd525d5ed0dd4146d1e65e9e225c
Robert Abreu <robertabreu@pursuit.org>
2019-05-01 16:09:05 -0400

    imported splash to app js, splash is a loadbuffer for the app while it fetches data

--- src/App.js
+++ src/App.js
@@ -1,54 +1,75 @@
 import React, { Component } from 'react';
-import Login from './containers/Login'
-import SignUp from './containers/SignUp';
-import Home from './containers/Home';
+import { Home, Login, Logout, Post, SignUp } from './containers/';
 import NavBar from './components/NavBar';
-import Logout from './containers/Logout';
-import Profile from './components/Profile'
+
+import Profile from './components/Profile';
+import Splash from './components/Splash';
 import { Grid } from '@material-ui/core';
 import { HashRouter, Route } from 'react-router-dom';
 import firebase from './firebase';
 
 // Context
-import {Provider} from './contexts/Auth';
+import { Provider } from './contexts/Auth';
 
 
 class App extends Component {
 
   state = {
     user: null,
-  }
+    loadBuffer: true,
+  };
+
+  updateUser = (user) => {
+
+  };
+  manageUser = (firebaseUser) => {
+    this.setState({ user: firebaseUser });
+
+  };
 
   componentDidMount() {
     this.unsubscribe = firebase.auth().onAuthStateChanged((user) => {
       if (user) {
-        this.setState({ user });
+        this.manageUser(user)
       }
       else {
         this.setState({ user: null })
       }
     })
+    // using a loadbuff for splash
+    setTimeout(() => {
+      this.setState({
+        loadBuffer: false,
+      })
+    }, 1500);
   }
+
 
   componentWillUnmount() {
     this.unsubscribe()
   }
 
   render() {
+    const { loadBuffer } = this.state;
     return (
       <>
-        <HashRouter>
-          <Provider value={this.state.user}>
-            <Route path='/' component={NavBar} />
-            <Grid container style={{ marginTop: '65px' }}>
-              <Route path='/' exact component={Home} />
-              <Route path='/profile' exact component={Profile} />
-              <Route path='/login' exact component={Login} />
-              <Route path='/signup' exact component={SignUp} />
-              <Route path='/logout' exact component={Logout} />
-            </Grid>
-          </Provider>
-        </HashRouter>
+        {
+          (loadBuffer) ? <Splash />
+            :
+            <HashRouter>
+              <Provider value={this.state.user}>
+                <Route path='/' component={NavBar} />
+                <Grid container style={{ marginTop: '65px' }}>
+                  <Route path='/' exact component={Home} />
+                  <Route path='/profile' exact component={Profile} />
+                  <Route path='/post' exact component={Post} />
+                  <Route path='/login' exact component={Login} />
+                  <Route path='/signup' exact component={SignUp} />
+                  <Route path='/logout' exact component={Logout} />
+                </Grid>
+              </Provider>
+            </HashRouter>
+        }
       </>
     );
   }
